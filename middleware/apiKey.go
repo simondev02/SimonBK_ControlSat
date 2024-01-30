a6fb076477f2b79d29b53977a7a774655b858162
sebastian.camacho <sebastian.camacho@finanzauto.com.co>
2024-01-30 14:56:13 -0500

    api finandina final

--- middleware/apiKey.go
+++ middleware/apiKey.go
@@ -1,6 +1,7 @@
 package middleware
 
 import (
+	"encoding/base64"
 	"net/http"
 	"os"
 
@@ -8,10 +9,14 @@ import (
 )
 
 func ValidateAPIKey() gin.HandlerFunc {
-	apiKey := os.Getenv("API_KEY") // Asegúrate de establecer esta variable de entorno
+	encodedApiKey := os.Getenv("API_KEY") // Asegúrate de establecer esta variable de entorno
+	apiKey, err := base64.StdEncoding.DecodeString(encodedApiKey)
+	if err != nil {
+		panic("Error al decodificar la API Key: " + err.Error())
+	}
 	return func(c *gin.Context) {
 		providedApiKey := c.GetHeader("X-API-Key")
-		if providedApiKey != apiKey {
+		if providedApiKey != string(apiKey) {
 			c.JSON(http.StatusUnauthorized, gin.H{"error": "API key no válida o ausente"})
 			c.Abort()
 		} else {
